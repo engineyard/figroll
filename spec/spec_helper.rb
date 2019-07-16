24fb3e7c2b4f1f05351b091ea4e95c85658aa013
Dennis Walters <pooster@gmail.com>
2019-07-16 16:50:37 -0400

    Updated simplecov config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,9 @@
 unless RUBY_VERSION =~ /^1\.8\./
   require 'simplecov'
-  SimpleCov.coverage_dir 'coverage/inside'
+  SimpleCov.coverage_dir 'coverage'
   SimpleCov.start do
     add_filter '/spec/'
-    add_filter '/features/'
     add_filter '/mock/'
-    add_filter '/lib/vendor/'
-    add_group 'CLI Workflows', 'lib/engineyard-serverside/cli/workflows/'
   end
 end
 
