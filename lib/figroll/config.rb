99a16a52d456d55a74064fe9dc41bf7d5c648884
Dennis Walters <pooster@gmail.com>
2019-07-21 13:31:43 -0400

    Added API documentation for `Figroll::Config`

--- lib/figroll/config.rb
+++ lib/figroll/config.rb
@@ -2,13 +2,33 @@ require 'yaml'
 require 'figroll/util'
 
 module Figroll
+  # A configuration object for Figroll
   class Config
-    attr_reader :required, :environment, :data
 
+    # A list of required environment variables defined by the configuration
+    # @return [Array<String>, nil]
+    # @api private
+    attr_reader :required
+
+    # The `FIGROLL_ENV` under which we're running
+    # @return [String, nil]
+    # @api private
+    attr_reader :environment
+
+    # Values defined in the configuration to inject into Figroll
+    # @return [Array<String>, nil]
+    # @api private
+    attr_reader :data
+
+    # Create a new Config instance
+    # @api private
     def initialize
       reset
     end
 
+    # Given a config file name, load the configuration specified in that file.
+    # @param config_file [String]
+    # @api private
     def load_file(config_file)
       return unless File.exists?(config_file)
 
